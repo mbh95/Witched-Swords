d3657454cabb2f87d824bbbec5d214ec979d36b8
matthewhammond <mbh2@rice.edu>
2017-01-14 18:37:42 -0600

    Fixed some naming errors and the bridge not being traversable.

--- core/src/com/comp460/screens/TacticsScreen.java
+++ core/src/com/comp460/screens/TacticsScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -29,6 +30,12 @@ public class TacticsScreen extends ScreenAdapter {
     private Main game;
     private OrthographicCamera camera;
 
+    private int mapWidth;
+    private int mapHeight;
+
+    private int tileWidth;
+    private int tileHeight;
+
     private TiledMap tiledMap;
     private OrthogonalTiledMapRenderer mapRenderer;
 
@@ -36,7 +43,7 @@ public class TacticsScreen extends ScreenAdapter {
     private int cursorDelay = 10;
     private int cursorRow, cursorCol;
 
-    private boolean[][] collisionMask;
+    private boolean[][] traversableMask;
     private Entity[][] mapContents;
 
     public TacticsScreen(Main parentGame, TiledMap tiledMap) {
@@ -47,22 +54,25 @@ public class TacticsScreen extends ScreenAdapter {
         this.tiledMap = tiledMap;
         this.mapRenderer = new OrthogonalTiledMapRenderer(Assets.testMap);
 
-        int width = tiledMap.getProperties().get("width", Integer.class);
-        int height = tiledMap.getProperties().get("height", Integer.class);
-        this.collisionMask = new boolean[height][width];
-        this.mapContents = new Entity[height][width];
+        mapWidth = tiledMap.getProperties().get("width", Integer.class);
+        mapHeight = tiledMap.getProperties().get("height", Integer.class);
 
-        // does stuff <3 <3 <3
+        tileWidth = tiledMap.getProperties().get("tilewidth", Integer.class);
+        tileHeight = tiledMap.getProperties().get("tileheight", Integer.class);
+
+        this.traversableMask = new boolean[mapHeight][mapWidth];
+        this.mapContents = new Entity[mapHeight][mapWidth];
+
+        // Iterate over map layers and create a collision mask for the map taking collision data from the topmost layer.
         for (MapLayer ml : Assets.testMap.getLayers()) {
             TiledMapTileLayer tl = (TiledMapTileLayer)ml;
-
-            for (int r = 0; r < height; r++) {
-                for (int c = 0; c < width; c++) {
+            for (int r = 0; r < mapHeight; r++) {
+                for (int c = 0; c < mapWidth; c++) {
                     TiledMapTileLayer.Cell cell = tl.getCell(c, r);
                     if (cell == null || cell.getTile() == null) {
                         continue;
                     }
-                    this.collisionMask[r][c] = cell.getTile().getProperties().get("traversable", Boolean.class);
+                    this.traversableMask[r][c] = cell.getTile().getProperties().get("traversable", Boolean.class);
                 }
             }
         }
@@ -115,25 +125,46 @@ public class TacticsScreen extends ScreenAdapter {
     private void drawMap() {
         this.mapRenderer.setView(this.camera);
         this.mapRenderer.render();
+    }
 
+    private void drawGridLines() {
         ShapeRenderer sr = new ShapeRenderer();
-        TiledMap map = mapRenderer.getMap();
-        int tileWidth = map.getProperties().get("tilewidth", Integer.class), tileHeight = map.getProperties().get("tileheight", Integer.class);
-        int mapWidth = map.getProperties().get("width", Integer.class) * tileWidth, mapHeight = map.getProperties().get("height", Integer.class) * tileHeight;
         sr.setProjectionMatrix(camera.combined);
         sr.setColor(Color.GRAY);
         sr.begin(ShapeRenderer.ShapeType.Line);
-        for(int x = 0; x < mapWidth; x += tileWidth)
-            sr.line(x, 0, x, mapHeight);
-        for(int y = 0; y < mapHeight; y += tileHeight)
-            sr.line(0, y, mapWidth, y);
+        for(int x = 0; x < mapWidth * tileWidth; x += tileWidth)
+            sr.line(x, 0, x, mapHeight * tileHeight);
+        for(int y = 0; y < mapHeight * tileHeight; y += tileHeight)
+            sr.line(0, y, mapWidth * tileWidth, y);
         sr.end();
+    }
+
+    private void drawCollisionMask() {
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        ShapeRenderer sr = new ShapeRenderer();
+        sr.setProjectionMatrix(camera.combined);
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        for (int r = 0; r < mapHeight; r++) {
+            for (int c = 0; c < mapWidth; c++) {
+                if (this.traversableMask[r][c]) {
+                    sr.setColor(0.0f, 0.0f, 1.0f, 0.3f);
+                } else {
+                    sr.setColor(1.0f, 0.0f, 0.0f, 0.3f);
+                }
+                sr.rect(c * tileWidth, r * tileHeight, tileWidth, tileHeight);
+            }
+        }
+        sr.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 
     @Override
     public void render(float delta) {
         update(delta);
         drawMap();
+        drawCollisionMask();
+        drawGridLines();
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
